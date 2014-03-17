f3ecd85d9c876048c16b1ff2d36abe6c96ca5814
Kevin <klee.taurus@gmail.com>
2014-03-17 17:36:24 +0800

    Add URLConfig class

---
+++ Java-RT-Training/src/com/fenby/practice/crawl/URLConfig.java
@@ -0,0 +1,26 @@
+/**
+ * --------------------------------------------------------------------
+ * Copyright (c) 2014 by VIPHold Technology.
+ * This software is the proprietary information of VIPHold Technology
+ * All Right Reserved.
+ * --------------------------------------------------------------------
+ */
+package com.fenby.practice.crawl;
+
+public class URLConfig {
+    private String code;
+    private String url;
+
+    public String getCode() {
+        return code;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    }
+    public String getUrl() {
+        return url;
+    }
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
